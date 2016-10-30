f55f21be09c255ec6bc54e39f014d8db5c697e52
Ivan Shamatov <status.enable@gmail.com>
2016-10-30 15:08:47 +0300

    initial commit

---
+++ spec/crossover_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Crossover do
+  it 'has a version number' do
+    expect(Crossover::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
